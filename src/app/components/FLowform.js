e3f70f044ed7bf30f53b73e8e472ea964b3d2a27
Umair <umairjavaid01@gmail.com>
2023-09-07 13:33:23 +0500

    Single Webpage using flowbites and NextJS13

---
+++ src/app/components/FLowform.js
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState } from "react";
+
+const FLowform = () => {
+  const [input, setInput] = useState({ name: "", email: "", password: "" });
+  const [first, setFirst] = useState([]);
+  // console.log(first, "Onsubmit");
+  // console.log(input);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFirst((fir) => {
+      return [...fir, input];
+    });
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInput((input) => {
+      return {
+        ...input,
+        [name]: value,
+      };
+    });
+  };
+  return (
+    <div className="md:w-[800px] p-20 m-auto">
+      <h1 className="mt-5 text-center text-xl mb-5">Form</h1>
+      <form className="mt-5" onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Name
+          </label>
+          <input
+            type="name"
+            name="name"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your name here..."
+            required
+            value={input.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="name@email.com"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            value={input.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FLowform;
